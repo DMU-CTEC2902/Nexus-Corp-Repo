3a3db7a0971db632121cd1e9aac96592b42dfa5d
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-02-13 15:12:53 +0000

    new button

--- Example/Example/Form1.Designer.cs
+++ Example/Example/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -59,11 +61,21 @@
             this.lblHelloWorld.TabIndex = 2;
             this.lblHelloWorld.Text = "Hello World";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(431, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.lblHelloWorld);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
@@ -79,6 +91,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label lblHelloWorld;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
