c81c6ad9d7694a7adaca32411d3271eb26e62d4a
Bhakti Mistry <P17189539@my365.dmu.ac.uk>
2019-03-04 16:02:28 +0000

    created the full MVC solution

---
+++ NexusCorpFilmReviews/NexusCorpFilmReviews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NexusCorpFilmReviews.Startup))]
+namespace NexusCorpFilmReviews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
