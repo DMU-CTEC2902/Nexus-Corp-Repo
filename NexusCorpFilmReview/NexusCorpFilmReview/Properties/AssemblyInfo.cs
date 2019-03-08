17977290a64d07e2495f06c6fff8f8e00947969b
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-03-08 14:09:36 +0000

    Models added

--- NexusCorpFilmReview/NexusCorpFilmReview/Properties/AssemblyInfo.cs
+++ NexusCorpFilmReview/NexusCorpFilmReview/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7e5d755b-bcbe-4fe8-81b4-13981ae691ad")]
+[assembly: Guid("1d209c30-ee64-44f1-94b7-f9d549525c7b")]
 
 // Version information for an assembly consists of the following four values:
 //
