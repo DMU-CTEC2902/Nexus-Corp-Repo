b5f29ae52b7ae3d088ba1ce9de9c966c408485cd
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-03-08 14:33:40 +0000

    CONToller + view testing

---
+++ NexusCorpFilmReview/NexusCorpFilmReview/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NexusCorpFilmReview.Models;
+
+namespace NexusCorpFilmReview.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ReviewModel review = new ReviewModel()
+            {
+                ReviewID = 1,
+                Review = "This was an excelent film! Loved it",
+                ReviewDateAdded = DateTime.Now.Date,
+                UserID = 23,
+                FilmID = 13
+
+            };
+
+            return View(review);
+        }
+    }
+}
---
+++ NexusCorpFilmReview/NexusCorpFilmReview/Models/GenreModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NexusCorpFilmReview.Models
+{
+    public class GenreModel
+    {
+        //Public property for the Genre ID in the Genre model. This is the primary key
+        public virtual int GenreID { get; set; }
+
+        //Public property for the Genre in the Genre model
+        public virtual string Genre { get; set; }
+    }
+}
