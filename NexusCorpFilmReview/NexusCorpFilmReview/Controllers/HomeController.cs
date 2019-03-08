cea72aa1a23b6732fe994d9296fded587c1b70a5
NnekaLy <44840139+Nne123@users.noreply.github.com>
2019-03-08 13:11:16 +0000

    Solution with Views + controllers
    Solution with views and controllers working

---
+++ NexusCorpFilmReview/NexusCorpFilmReview/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NexusCorpFilmReview.Models;
+
+namespace NexusCorpFilmReview.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            NewsStoryModel newsStory = new NewsStoryModel()
+            {
+                Headline = "Phew what a scorcher",
+                ByLine = "Ron Journo",
+                Content = "British people are obsessed with the weather"
+            };
+
+            return View(newsStory);
+        }
+    }
+}
---
+++ NexusCorpFilmReview/NexusCorpFilmReview/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NexusCorpFilmReview.MvcApplication" Language="C#" %>
