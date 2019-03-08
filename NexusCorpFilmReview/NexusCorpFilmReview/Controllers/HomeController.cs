67f53ced1e2743a311a7874b12fd4fe89ccb0198
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-03-08 18:26:19 +0000

    Nexus Corp FIlm Review  + controller/views work

--- NexusCorpFilmReview/NexusCorpFilmReview/Controllers/HomeController.cs
+++ NexusCorpFilmReview/NexusCorpFilmReview/Controllers/HomeController.cs
@@ -3,26 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using NexusCorpFilmReview.Models;
 
 namespace NexusCorpFilmReview.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
-            ReviewModel review = new ReviewModel()
-            {
-                ReviewID = 1,
-                Review = "This was an excelent film! Loved it",
-                ReviewDateAdded = DateTime.Now.Date,
-                UserID = 23,
-                FilmID = 13
+            return View();
+        }
 
-            };
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
 
-            return View(review);
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
     }
 }
