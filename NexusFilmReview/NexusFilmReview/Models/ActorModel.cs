1120277b2c96e8927ff169ead73fa986a9b24638
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-03-10 16:51:12 +0000

    Controllers + Add view works

--- NexusFilmReview/NexusFilmReview/Models/ActorModel.cs
+++ NexusFilmReview/NexusFilmReview/Models/ActorModel.cs
@@ -7,5 +7,19 @@ namespace NexusFilmReview.Models
 {
     public class ActorModel
     {
+        //Public property for the Actor ID in the Actor Model. This is a primary key
+        public virtual int ActorID { get; set; }
+
+        //Public property for the Actor First Name in the Actor Model
+        public virtual string ActorFirstName { get; set; }
+
+        //Public property for the Actor Last Name in the Actor Model
+        public virtual string ActorLastName { get; set; }
+
+        //Public property for the Actor Date of Birth in the Actor Model
+        public virtual DateTime ActorDOB { get; set; }
+
+        //Public property for the Actor Gender in the Actor Model
+        public virtual string ActorGender { get; set; }
     }
 }
--- NexusFilmReview/NexusFilmReview/Models/GenreModel.cs
+++ NexusFilmReview/NexusFilmReview/Models/GenreModel.cs
@@ -7,5 +7,10 @@ namespace NexusFilmReview.Models
 {
     public class GenreModel
     {
+        //Public property for the Genre ID in the Genre model. This is the primary key
+        public virtual int GenreID { get; set; }
+
+        //Public property for the Genre in the Genre model
+        public virtual string Genre { get; set; }
     }
 }
