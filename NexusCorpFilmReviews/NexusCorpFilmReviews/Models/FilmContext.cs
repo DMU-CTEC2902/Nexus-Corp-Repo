3a6597bb44b451f068fc9708c6eed56af8dce32a
Bhakti Mistry <P17189539@my365.dmu.ac.uk>
2019-03-11 11:25:54 +0000

    Corrected errors

---
+++ NexusCorpFilmReviews/NexusCorpFilmReviews/Models/FilmContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NexusCorpFilmReviews.Models
+{
+    public class FilmContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public FilmContext() : base("name=FilmContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<NexusCorpFilmReviews.Models.Film> Films { get; set; }
+
+        public System.Data.Entity.DbSet<NexusCorpFilmReviews.Models.Credit> Credits { get; set; }
+
+        public System.Data.Entity.DbSet<NexusCorpFilmReviews.Models.Genre> Genres { get; set; }
+
+        public System.Data.Entity.DbSet<NexusCorpFilmReviews.Models.User> Users { get; set; }
+    }
+}
