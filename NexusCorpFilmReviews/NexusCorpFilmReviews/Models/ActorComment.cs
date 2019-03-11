618b16ab6e1afdf3c2154fabf353f1973820b9d8
P17218368 <P17218368@GWHXX653WIN005W>
2019-03-11 16:54:23 +0000

    awlkdwaolid

--- NexusCorpFilmReviews/NexusCorpFilmReviews/Models/ActorComment.cs
+++ NexusCorpFilmReviews/NexusCorpFilmReviews/Models/ActorComment.cs
@@ -10,17 +10,20 @@ namespace NexusCorpFilmReviews.Models
         //Public property for the Actor Comment ID in the Comment Model. This is the primary key
         public virtual int ActorCommentId { get; set; }
 
+        //Public property for the Actor ID in the Comment Model. This is the primary key
+        public virtual int ActorId { get; set; }
+
         //Public property for the Actor Comment in the Comment Model
         public virtual string ActorCommentContent { get; set; }
 
         //Public property for the Actor Comment Date Added in the Comment Model
         public virtual DateTime ActorCommentDateAdded { get; set; }
 
-        //Public property for the User Name in the Review Model. 
-        public virtual int UserName { get; set; }
+        //Public property for the User Name in the Actor Comment Model. 
+        public virtual string UserName { get; set; }
 
         //Connecting the foreign keys to this model
 
-        public virtual Actor ActorId { get; set; }
+        public virtual Actor Actor { get; set; }
     }
 }
