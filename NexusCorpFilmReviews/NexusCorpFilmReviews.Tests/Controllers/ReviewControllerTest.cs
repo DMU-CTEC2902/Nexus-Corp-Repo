93ea6448bb266556012a57a293b0702e0693de93
Bhakti Mistry <P17189539@my365.dmu.ac.uk>
2019-03-25 17:39:01 +0000

    create test for the controllers

---
+++ NexusCorpFilmReviews/NexusCorpFilmReviews.Tests/Controllers/ReviewControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NexusCorpFilmReviews.Controllers;
+using System.Web.Mvc;
+using NexusCorpFilmReviews.Models;
+
+namespace NexusCorpFilmReviews.Tests.Controllers
+{
+    [TestClass]
+    public class ReviewControllerTest
+    {
+        //[TestMethod]
+        //public void Index()
+        //{
+        //    //Arrange
+        //    ReviewsController controller = new ReviewsController();
+
+        //    // Act
+        //    ViewResult result = controller.Index() as ViewResult;
+
+        //    // Assert
+        //    Assert.IsNotNull(result);
+        //}
+
+        //[TestMethod]
+        //public void Details()
+        //{
+        //    //Arrange
+        //    ReviewsController controller = new ReviewsController();
+
+        //    //created an object to hold a film
+        //    Review newReview = new Review();
+        //    // Act
+        //    ViewResult Review = controller.Details(1) as ViewResult;
+        //    //places the details found in details 1 into the new object
+        //    newReview = Review.Model as Review;
+        //    // Assert
+        //    Assert.AreEqual(newReview.ReviewContent, "The graphics of the films are amazing!!");
+        //}
+
+    }
+}
